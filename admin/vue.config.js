8287d396dc0bd950713a3a9258171b45427f5bbc
Dmitry Erzunov <derzunov.work@yandex.com>
2023-05-25 13:49:59 +0300

    Режим сборки под локальный сервер. localbuild

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,8 +1,16 @@
 const { defineConfig } = require( '@vue/cli-service' )
+
+const getPublicPath = () => {
+  switch ( process.env.NODE_ENV ) {
+    case 'production': return '/tools/admin/' // production path -  the path we'll deploy to ( `url.ru/tools/admin/` )
+    case 'development': return '/tools/admin/' // local development url localhost:8080/THIS_PATH
+    case 'localbuild': return '/tools/admin/build/' // Apache/etc local build
+    default: return '/tools/admin/'
+  }
+}
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/tools/admin/' // production path -  the path we'll deploy to ( `url.ru/tools/admin/` )
-      : '/tools/admin/', // local development url localhost:8080/THIS_PATH
+  publicPath: getPublicPath(),
   outputDir: 'build',  // path to results of `build` command
 })
