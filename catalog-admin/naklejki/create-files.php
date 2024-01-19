11639141067364f1f897c82d191d41d476d71c43
Dmitry Erzunov <derzunov.work@yandex.com>
2024-01-19 08:08:25 +0300

    Создание картинки товара из канвы для листинга товаров

--- catalog-admin/naklejki/create-files.php
+++ catalog-admin/naklejki/create-files.php
@@ -6,8 +6,19 @@
 
     header( 'Content-Type: application/json; charset=utf-8' );
 
-    $fd = fopen( "./tovary/" . $_POST['fileName'] . ".json", 'w' ) or die( "Can\'t create/update in /tovary/" );
+
+    // Пишем json-файл
+    $fd = fopen( "./tovary/" . $_POST['fileName'] . ".json", 'w' ) or die( "Can\'t create/update json in /tovary/" );
     fwrite( $fd, $_POST['json'] );
+    fclose( $fd );
+
+    // Пишем картинку
+    $imageData = $_POST[ 'imageData' ];
+    $imageData = str_replace( 'data:image/jpeg;base64,', '', $imageData );
+    $imageData = str_replace( ' ', '+', $imageData );
+    $decodedData = base64_decode( $imageData );
+    $fd = fopen( "./tovary/" . $_POST['fileName'] . ".jpg", 'w' ) or die( "Can\'t create/update jpeg in /tovary/" );
+    fwrite( $fd, $decodedData );
     fclose( $fd );
 
     // Отвечаем в формате JSON (Для наглядности в Network->Preview в DevTools)
